c135b33665979ed5494ff4c43e6131f246c68380
alessandro96 <alex.renzi@hotmail.it>
2020-04-15 14:55:20 +0200

    Creato progetto

---
+++ src/models/Die.java
@@ -0,0 +1,17 @@
+package models;
+
+public class Die {
+	private int face;
+
+	public int getFace() {
+		return face;
+	}
+
+	public void setFace(int face) {
+		this.face = face;
+	}
+	
+	public Die(int face) {
+		this.face = face;
+	}
+}
